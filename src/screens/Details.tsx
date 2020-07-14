716c4978dcbda5efa289159729624c83c1faa6bc
Szpakusik <szpakusik@gmail.com>
2020-07-14 18:16:04 +0200

    handle landscape in details

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,21 +1,43 @@
 import * as React from 'react';
-import { View, Text, Image, Dimensions} from 'react-native';
+import { View, Text, Image, Dimensions, StyleSheet} from 'react-native';
 
-export function DetailsScreen({route, navigation}: { route:any, navigation: any }) {
+export function DetailsScreen({ route }: { route:any }) {
 
-    let screenHeight: number;
-    screenHeight = Dimensions.get('screen').height;
+    const [ screenHeight, setScreenHeight] = React.useState( Dimensions.get('screen').height )
+    const [ screenWidth, setScreenWidth] = React.useState( Dimensions.get('screen').width )
 
+    Dimensions.addEventListener('change', () => {
+      setScreenHeight(Dimensions.get('screen').height);
+      setScreenWidth(Dimensions.get('screen').width)
+    });
+    
     return (
-        
-    route.params ? 
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Image source = { {uri:route.params.img} } style = {{
-                            width: 360,
-                            height: screenHeight * 0.85,
-                            resizeMode: 'contain'
-                        }}/>
-      </View>
-      : null
+        route.params ? 
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+          <Image  source = { {uri:route.params.img} } 
+                  style = {{
+                    marginTop: 10,
+                    width: screenWidth * 0.98,
+                    height: screenHeight * 0.70,
+                    resizeMode: 'contain'
+                  }}
+          />
+          {screenWidth < 479 ?
+          <Text style={styles.tip}>Rotate for a better view!</Text>
+          : null}
+
+        </View>
+        : null
     );
+}
+const styles = StyleSheet.create({
+  tip:{
+      fontSize: 10,
+      textAlign:"center",
+      marginTop: 30,
+      marginBottom: 5,
+      backgroundColor: "orange",
+      padding: 15,
   }
+})
